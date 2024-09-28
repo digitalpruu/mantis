16b0468039e8e29feaba58fd9365ff09e53216a9
kevinpicon01 <digitalpruu@gmail.com>
2024-09-27 19:15:12 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,10 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Mantis";
+const email = "estibensantiago@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    "¡Hola! Estoy interesado en conocer más sobre los productos de " + title + " y me gustaría obtener más información. ¡Gracias!"
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "+573107674971";
+
 const textos = {
     index: {
         title: title,
@@ -12,25 +13,26 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+
+        description1: "En Mantis, nos dedicamos a ofrecer ropa de alta calidad que se adapta a todos los estilos. Creemos que la moda debe ser accesible y satisfactoria para todos. Nuestro lema, \"Lo que no te guste, no pagas,\" es una promesa de que siempre priorizaremos la satisfacción del cliente.",
+        description2: "Trabajamos arduamente para proporcionar prendas que no solo sean elegantes, sino también cómodas y duraderas. Nos enorgullece ofrecer un servicio al cliente excepcional, asegurándonos de que cada cliente se sienta valorado y atendido."
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Lo que no te guste no pagas.",
+            p2: ""
         },
-        address: "Dirección del emprendimiento",
+        address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/jhoiner.santiago.98?mibextid=ZbWKwL",
+        instagram: "www.instagram.com", // Instagram no proporcionado
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
+
 export default textos;
